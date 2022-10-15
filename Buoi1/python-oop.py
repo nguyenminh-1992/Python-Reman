2400e78c69aee903ecf01bfc21e057a3d450e02a
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-10-15 20:57:01 +0700

    oop3

--- Buoi1/python-oop.py
+++ Buoi1/python-oop.py
@@ -24,14 +24,20 @@ class Reman1(BKACAD,Person):
     def __init__(self,name,age):
         self.name = name
         self.age = age
+    def in_thong_tin(self):
+        print("Hoc vien lop Reman1")
    
 
 class Reman2(BKACAD):
     def __init__(self,name,age):
         self.name = name
         self.age = age
+    def in_thong_tin(self):
+        print("Hoc vien lop Reman2")
 
-    
+def kiemtra(sv):
+    sv.in_thong_tin()
+
 student1 = Reman1('Quang',20)
 student2 = Reman2('Vinh',30)
 
@@ -39,3 +45,6 @@ student1.study()
 student2.study()
 student1.sleep()
 student1.eat()
+
+kiemtra(student1)
+kiemtra(student2)
