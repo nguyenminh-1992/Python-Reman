a74c623be5fc9d4be9053ecfc1666ae04a3db812
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-10-15 21:26:05 +0700

    baitap

---
+++ Buoi1/python-baitap.py
@@ -0,0 +1,18 @@
+#Viết chương trình nhập thông tin lớp Python
+#Bước 1: Nhấp số học viên
+#Bước 2: Điền thông tin mỗi học viên (tên, tuổi, quê quán)
+#Bước 3: Hiển thị danh sách lớp Python
+
+#Ví dụ: 
+#B1: 3 -> 3 học viên
+#B2: Nhập 
+#B3: Nguyen Van A - 20 - Ha Noi
+# Nguyen Van B - 21 - HCM
+# Nguyen Van C - 22 - Hue
+
+#Tạo Class Python, định nghĩa các thông tin ở __init__
+#Khai bao 1 List 
+#In ra theo For
+# student1, student2, student3
+# A = [student1, student2, student3]
+#student1.name, student1.age, student1.country
