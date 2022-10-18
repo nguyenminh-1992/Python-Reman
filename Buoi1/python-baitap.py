addca69951e364fe696581fc89305383312f7708
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-10-18 20:11:48 +0700

    baitap1

--- Buoi1/python-baitap.py
+++ Buoi1/python-baitap.py
@@ -16,3 +16,28 @@
 # student1, student2, student3
 # A = [student1, student2, student3]
 #student1.name, student1.age, student1.country
+
+#C R U D
+# Create
+# Read
+# Update
+# Delete
+
+#Dinh nghia: Ten, tuoi, que quan
+#Create - Read
+
+class PythonReman:
+    def __init__(self,name,age,country):
+        self.name = name
+        self.age = age
+        self.country = country
+#Giong nhau -> For, While
+name1 = input("Nhap ten: ")
+age1 = int(input("Nhap tuoi: "))
+country1 = input("Nhap que quan: ")
+
+student1 = PythonReman(name1,age1,country1)
+
+n = int(input("Nhap sp luong hoc vien: "))
+
+
