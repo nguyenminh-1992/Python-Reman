ab074ee53ab4a777884bd7eb2b7382f9e8d7d47b
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-10-18 19:47:08 +0700

    modules1

---
+++ Buoi2/mainmodules.py
@@ -0,0 +1,13 @@
+#from tinhtoan import tinhtong, tinhhieu
+# from tinhtoan import *
+
+# a = tinhtong(5,6)
+# print(a)
+
+# b = tinhhieu(6,2)
+# print(b)
+
+#Cach khai bao 2
+import tinhtoan
+
+print(tinhtoan.tinhtong(3,4))
---
+++ Buoi2/tinhtoan.py
@@ -0,0 +1,9 @@
+def tinhtong(a,b):
+    return a+b
+
+def tinhhieu(a,b):
+    return a-b
+
+
+# c = tinhtong(2,4)
+# print(c)
