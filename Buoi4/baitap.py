f6831b0c0719d4a417cb955c3f623e3a70b80bdc
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-10-22 21:27:37 +0700

    baitap

---
+++ Buoi4/baitap.py
@@ -0,0 +1,14 @@
+
+
+
+# Chương trình quản lý nhân viên 
+# - Nhân viên: Mã nhân viên, Tên, tuổi, quê quán, chức vụ, giới tính (Nhập số), Chấm công (số ngày)
+# (Mã nhân viên: Tự sinh ra theo mẫu, chức vụ + số)
+# GD1, TP1, TP2, TP3
+# (Giới tính:0-Unknown 1-Male 2-Female)
+# (Chức vụ: GD-Giám đốc  TP-Trưởng phòng  CB-Cán bộ)
+# - Có các chức năng thêm nhân viên, sửa chức vụ, xoá nhân viên, hiển thị nhân viên, sắp xếp nhân viên
+# - Tạo thêm phần tính lương theo chấm công
+# Ví dụ: GD = chấm công x 1 triệu, TP = chấm công x 500k, CB = chấm công x 400k
+
+# Hiển thị: Mã nhân viên, Tên, tuổi, quê quán, chức vụ, giới tính, Chấm công, Tiền lương
