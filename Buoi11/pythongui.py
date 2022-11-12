46dad14fb27ea9ba8f0eea6c160c4480cf53b33c
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-11-12 19:23:53 +0700

    python gui

---
+++ Buoi11/pythongui.py
@@ -0,0 +1,15 @@
+# G U I
+
+# pip3 install tkinter
+import tkinter
+
+#Tao khung giao dien
+giaodien = tkinter.Tk()
+giaodien.title("Demo GUI")
+giaodien.geometry('500x400')
+
+
+
+
+
+giaodien.mainloop()
