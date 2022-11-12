219edfd0875a2890d80e69197a65f0ec6cd52472
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-11-12 19:51:20 +0700

    python gui 3

--- Buoi11/pythongui.py
+++ Buoi11/pythongui.py
@@ -3,6 +3,8 @@
 # pip3 install tkinter
 import tkinter
 from tkinter.ttk import Combobox
+from tkinter import Checkbutton, Radiobutton, IntVar, Spinbox
+from tkinter.scrolledtext import ScrolledText
 
 
 #Tao khung giao dien
@@ -27,6 +29,29 @@ combobox = Combobox(giaodien)
 combobox['values'] = (1,2,3,4,'Text')
 combobox.grid(column=2,row=3)
 
+#Checkbox
+checkbox1 = Checkbutton(giaodien,text="Lua chon 1")
+checkbox1.grid(column=2,row=4)
+
+checkbox2 = Checkbutton(giaodien,text="Lua chon 2")
+checkbox2.grid(column=2,row=5)
+
+#Radiobox
+select = IntVar()
+radiobox1 = Radiobutton(giaodien, text="Choose 1",variable=select)
+radiobox1.grid(column=2,row=6)
+
+select = IntVar()
+radiobox2 = Radiobutton(giaodien, text="Choose 2",variable=select)
+radiobox2.grid(column=2,row=7)
+
+#Scrolled Text
+scrolled = ScrolledText(giaodien,width=20,height = 10)
+scrolled.grid(column=2,row=8)
+
+#Spinbox
+spinbox = Spinbox(giaodien,from_=1,to=10,width=10)
+spinbox.grid(column=2,row=9)
 
 
 
